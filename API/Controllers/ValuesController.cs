fd0006adc30b5cf950d056564e1536351d5fa457
christian_glenn_Telus <christian_glenn.verdera@telus.com>
2020-07-09 18:46:08 -0400

    Add project files.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+
+        //GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            //return new string[] {"value1","value2"};
+            //synchronous call
+            //var values = _context.Values.ToList();
+
+            //async call
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            //async call
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+
+
+
+    }
+}
